7aba3dcbb2bb1864a5e4b2f10d439de8bb9ee0e4
Andrew Overton <aoverton@shopkeep.com>
2016-09-08 11:12:44 -0400

    Set up for accessory detection in Xcode 7

--- TestAccessoryManager/ViewController.m
+++ TestAccessoryManager/ViewController.m
@@ -8,20 +8,52 @@
 
 #import "ViewController.h"
 
+#import <ExternalAccessory/ExternalAccessory.h>
+
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *connectedAccessories;
+@property (weak, nonatomic) IBOutlet UILabel *connectionState;
+
 @end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [[EAAccessoryManager sharedAccessoryManager] registerForLocalNotifications];
+    
+    [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(updateForConnected:) name:EAAccessoryDidConnectNotification object:nil];
+    
+    [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(updateForDisconnected:) name:EAAccessoryDidDisconnectNotification object:nil];
+
+    [self refresh:nil];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)dealloc {
+    [NSNotificationCenter.defaultCenter removeObserver:self];
+}
+
+- (NSUInteger)connectedAccessoryCount {
+    return [EAAccessoryManager sharedAccessoryManager].connectedAccessories.count;
+}
+
+- (IBAction)refresh:(id)sender {
+    NSUInteger count = self.connectedAccessoryCount;
+    self.connectedAccessories.text = [NSString stringWithFormat:@"%lu", (unsigned long)self.connectedAccessoryCount];
+    count == 0 ? [self updateForDisconnected:nil] : [self updateForConnected:nil];
+}
+
+- (void)updateForConnected:(id)sender {
+    self.connectionState.text = @"Connected";
+    self.connectionState.textColor = UIColor.greenColor;
+}
+
+- (void)updateForDisconnected:(id)sender {
+    self.connectionState.text = @"Disconnected";
+    self.connectionState.textColor = UIColor.redColor;
 }
 
 @end
