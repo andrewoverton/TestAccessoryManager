131283f8d88a6444480d0969761bfce78a6c9fd4
Andrew Overton <aoverton@shopkeep.com>
2016-09-08 10:53:38 -0400

    First commit

---
+++ TestAccessoryManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAccessoryManager
+//
+//  Created by Andrew Overton on 9/8/16.
+//  Copyright © 2016 Overton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
